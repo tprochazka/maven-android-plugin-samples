b9d614dd766a21331bb9c7420f8314848990d924
William Ferguson <william.ferguson@xandar.com.au>
2014-01-19 16:59:23 +1000

    Added libraryprojects-aar-from-aar Added libraryprojects-aar-from-apklib Added ref to parent POM for each module and made the version of android-maven-plugin configured in dependency management only at the top level (currently 3.8.3-SNAPSHOT but suggest this be changed to a release version).

--- libraryprojects/libraryprojects-aar-from-aar/src/main/java/com/jayway/maven/plugins/android/generation2/samples/libraryprojects/aarFromAar/AarFromAarClass.java
+++ libraryprojects/libraryprojects-aar-from-aar/src/main/java/com/jayway/maven/plugins/android/generation2/samples/libraryprojects/aarFromAar/AarFromAarClass.java
@@ -5,7 +5,7 @@ import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.aar1
 /**
  * Extends lib1Class from Aar1Class to prove that we have compiled using aar1 as a dependency.
  */
-public class Aar2Class extends Aar1Class {
+public class AarFromAarClass extends Aar1Class {
 
     public String getString2() {
         return "String2";
--- libraryprojects/libraryprojects-aar-from-apklib/src/main/java/com/jayway/maven/plugins/android/generation2/samples/libraryprojects/aarFromApklib/AarFromApklibClass.java
+++ libraryprojects/libraryprojects-aar-from-apklib/src/main/java/com/jayway/maven/plugins/android/generation2/samples/libraryprojects/aarFromApklib/AarFromApklibClass.java
@@ -1,11 +1,11 @@
-package com.jayway.maven.plugins.android.generation2.samples.libraryprojects;
+package com.jayway.maven.plugins.android.generation2.samples.libraryprojects.aarFromApklib;
 
 import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.lib1.Lib1Class;
 
 /**
  * Extends lib1Class from apklib1 to prove that we have compiled using apklib1 as a dependency.
  */
-public class AarClass1 extends Lib1Class {
+public class AarFromApklibClass extends Lib1Class {
 
     public String getString2() {
         return "String2";
