9b39e7498506a099a1f60cfac429ec77e94aff17
William Ferguson <william.ferguson@xandar.com.au>
2014-01-20 22:29:42 +1000

    Added tests for all 4 dependencies within apk-with-deps.

--- libraryprojects/libraryprojects-apk-with-deps/src/main/java/com/jayway/maven/plugins/android/generation2/samples/libraryprojects/mainapp/MainActivity.java
+++ libraryprojects/libraryprojects-apk-with-deps/src/main/java/com/jayway/maven/plugins/android/generation2/samples/libraryprojects/mainapp/MainActivity.java
@@ -2,8 +2,9 @@ package com.jayway.maven.plugins.android.generation2.samples.libraryprojects.mai
 
 import android.os.Bundle;
 import android.widget.TextView;
-import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.aar1.AbstractActivityUsingResources;
 import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.aar1.Aar1Class;
+import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.aar1.AbstractActivityUsingResources;
+import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.aarFromAar.AarFromAarClass;
 import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.apklibFromApklib.ApklibFromApklibClass;
 import com.jayway.maven.plugins.android.generation2.samples.libraryprojects.lib1.Lib1Class;
 import org.apache.commons.io.IOUtils;
@@ -11,33 +12,36 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AbstractActivityUsingResources
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends AbstractActivityUsingResources {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        setTextFromClass(R.id.aar1javaclass, Aar1Class.getString());
-        setTextFromClass(R.id.lib1javaclass, Lib1Class.getString());
-        setTextFromClass(R.id.lib2javaclass, ApklibFromApklibClass.getString());
+        setTextFromClass(R.id.aar1Javaclass, Aar1Class.getString());
+        setTextFromClass(R.id.lib1Javaclass, Lib1Class.getString());
+        setTextFromClass(R.id.aarFromAarJavaclass, AarFromAarClass.getString());
+        setTextFromClass(R.id.apklibFromApklibJavaclass, ApklibFromApklibClass.getString());
 
-        setTextFromAsset(R.id.aar1asset, "aar1asset.txt");
-        setTextFromAsset(R.id.lib1asset, "lib1asset.txt");
-        setTextFromAsset(R.id.lib2asset, "lib2asset.txt");
-        
-        setTextFromResource(R.id.aar1javaresource, "/aar1javaresource.txt");
-        setTextFromResource(R.id.lib1javaresource, "/lib1javaresource.txt");
-        setTextFromResource(R.id.lib2javaresource, "/lib2javaresource.txt");
+        setTextFromAsset(R.id.aar1Asset, "aar1Asset.txt");
+        setTextFromAsset(R.id.lib1Asset, "lib1Asset.txt");
+        setTextFromAsset(R.id.aarFromAarAsset, "aarFromAarAsset.txt");
+        setTextFromAsset(R.id.apklibFromApklibAsset, "apklibFromApklibAsset.txt");
+
+        setTextFromResource(R.id.aar1JavaResource, "/aar1JavaResource.txt");
+        setTextFromResource(R.id.lib1JavaResource, "/lib1JavaResource.txt");
+        setTextFromResource(R.id.aarFromAarJavaResource, "/aarFromAarJavaResource.txt");
+        setTextFromResource(R.id.apklibFromApklibJavaResource, "/apklibFromApklibJavaResource.txt");
     }
-    
+
     private void setTextFromClass(int id, String text) {
         final TextView view = (TextView) findViewById(id);
         view.setText(text);
     }
-    
+
     private void setTextFromAsset(int id, String assetfilename) {
         final TextView view = (TextView) findViewById(id);
         try {
@@ -47,15 +51,14 @@ public class MainActivity extends AbstractActivityUsingResources
             e.printStackTrace();
         }
     }
-    
-    private void setTextFromResource(int id, String resourcePath) {
-      final TextView view = (TextView) findViewById(id);
-      try {
-          final InputStream inputStream = this.getClass().getResourceAsStream(resourcePath);
-          view.setText(IOUtils.toString(inputStream));
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-    }
 
+    private void setTextFromResource(int id, String resourcePath) {
+        final TextView view = (TextView) findViewById(id);
+        try {
+            final InputStream inputStream = this.getClass().getResourceAsStream(resourcePath);
+            view.setText(IOUtils.toString(inputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
